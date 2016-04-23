8967cabb9970d3b92622b7f2c8a037ad2ea0f6d3
Ailin Yu <axy98@case.edu>
2016-04-23 10:52:48 -0400

    Added CallbackTasks

---
+++ pi_trees_lib/CallbackTask.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ailin on 4/22/2016.
+//
+
+#include "CallbackTask.h"
---
+++ pi_trees_lib/headers/CallbackTask.h
@@ -0,0 +1,14 @@
+//
+// Created by Ailin on 4/22/2016.
+//
+
+#ifndef PI_TREES_CALLBACKTASK_H
+#define PI_TREES_CALLBACKTASK_H
+
+
+class CallbackTask {
+
+};
+
+
+#endif //PI_TREES_CALLBACKTASK_H
